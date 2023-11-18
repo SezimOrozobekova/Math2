4b684cc5be3572ccd7b2d71f9b97f49cb182d4fb
Your Name <you@example.com>
2023-11-18 18:50:27 +0600

    eureka

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import math
-n, a, error, i = 2, 9, 1e-6, 1
-a1 = pow(n, math.floor(math.log(9, 2)))
+n, a, error, i = 2, 8.9991, 1e-6, 1
+a1 = pow(n, math.floor(math.log(a, 2)))
 dif, a2, result = a - a1, 1, 0
 x = dif / a1
-while abs(math.log(2, 9) - (3+result)) > error:
+while abs(math.log(a, 2) - (math.log(a1, 2) + result)) > error:
     result += (((-1) ** (i + 1)) * (1 / math.log(2))) / i * (x ** i)
     i = i + 1
-    print(math.log(2, 9))
+print(result + math.log(a1, 2))
