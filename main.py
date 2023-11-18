3685b65eb8de7fb68b8eeb52e15d8886fcda9c3a
Your Name <you@example.com>
2023-11-18 13:20:58 +0600

    because of inf

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import math
+import mpmath
 n = 2
 a = 9
 error = 1e-6
 
 
 a_low = 8
-a2 = 1
-diff = a - a_low
-x = diff / a_low
+a2 = 8
+x = a - a2
 before = 1/a2
 k = -1
-result = 0
+result = mpmath.mpf(0)
 i = 1
-s_n = math.log(a2, n)
+s_n = mpmath.log(a2, n)
+
 
 while abs(math.log(a, n) - s_n) > error:
     if i == 1:
-        result = round(before * math.log(n) ** k, 6)
-        s_n = s_n + result / math.factorial(i) * (x ** i)
-        s_n = s_n * 3
+        result = round(before * mpmath.log(n) ** k, 6)
+        s_n = s_n + result / mpmath.factorial(i) * (x ** i)
     else:
-        result = round(before * k * (math.log(n) ** (k-1)) * (1/n), 6)
-        s_n = s_n + result / math.factorial(i) * (x ** i)
-        s_n = s_n * 3
+        result = round(before * k * (mpmath.log(n) ** (k-1)) * (1/n), 6)
+        s_n = s_n + result / mpmath.factorial(i) * (x ** i)
         before = before * k * (1 / n)
         k = k - 1
     i = i + 1
