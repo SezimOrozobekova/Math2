afda659180b36c784e71341689be51712c5cb7dd
Your Name <you@example.com>
2023-11-18 18:42:13 +0600

    wrode work

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 import math
-import mpmath
-n = 2
-a = 9
-error = 1e-6
-
-
-a_low = 8
-a2 = 8
-x = a - a2
-before = 1/a2
-k = -1
-result = mpmath.mpf(0)
-i = 1
-s_n = mpmath.log(a2, n)
-
-
-while abs(math.log(a, n) - s_n) > error:
-    if i == 1:
-        result = round(before * mpmath.log(n) ** k, 6)
-        s_n = s_n + result / mpmath.factorial(i) * (x ** i)
-    else:
-        result = round(before * k * (mpmath.log(n) ** (k-1)) * (1/n), 6)
-        s_n = s_n + result / mpmath.factorial(i) * (x ** i)
-        before = before * k * (1 / n)
-        k = k - 1
+n, a, error, i = 2, 9, 1e-6, 1
+a1 = pow(n, math.floor(math.log(9, 2)))
+dif, a2, result = a - a1, 1, 0
+x = dif / a1
+while abs(math.log(2, 9) - (3+result)) > error:
+    result += (((-1) ** (i + 1)) * (1 / math.log(2))) / i * (x ** i)
     i = i + 1
-    print("Derivative of", i, "is", result)
-    print("Sum of Teylor series is", s_n)
+    print(math.log(2, 9))
